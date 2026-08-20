9bed8a8ec04470be8429b73435166756e9672ffe
Ajitbabu T Y <107815296+ajitbabu10@users.noreply.github.com>
2026-08-20 20:23:52 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.java
@@ -0,0 +1,26 @@
+class MedianFinder {
+
+    List<Integer> list;
+
+    public MedianFinder() {
+        list = new ArrayList<>();
+    }
+    
+    public void addNum(int num) {   
+        list.add(num);
+        Collections.sort(list);
+    }
+    
+    public double findMedian() {
+        if(list.size() % 2 == 1) {
+            int mid = list.size() / 2;
+            double ans = list.get(mid);
+            return ans;
+        } else {
+            int mid1 = list.size() / 2;
+            int mid2 = mid1 - 1;
+            double ans = (list.get(mid1) + list.get(mid2))/2.0;
+            return ans;
+        }
+    }
+}
